c374e9ef77ce39985102c474786b0343b5d0e64c
Camila Palma <cpalma.contacto@gmail.com>
2024-01-31 15:09:23 -0300

    cajaaño y btnreset

--- src/buscador.js
+++ src/buscador.js
@@ -1,19 +1,33 @@
 //contenedor root
 const rootElement = document.getElementById("root");
 
+  //textarea titulo
+  const cajaTexto = document.createElement("textarea");
+  cajaTexto.placeholder = "Titulo";
+  rootElement.appendChild(cajaTexto);
+
+//textarea Año
+const cajaYear = document.createElement("textarea");
+  cajaYear.placeholder = "Año";
+  rootElement.appendChild(cajaYear);
+
 // Boton buscar
 const searchButton = document.createElement("button");
 searchButton.textContent = "Buscar";
 searchButton.addEventListener("click", () => {
   console.log("buscar las pelis");
 });
-
 // Obtener el elemento con el id "root" y agrega el botón
 rootElement.appendChild(searchButton);
 
-  //textarea
-  const cajaTexto = document.createElement("textarea");
-  cajaTexto.placeholder = "Ingresa una película...";
-  // Agregar la caja de texto al elemento con id "root"
-  rootElement.appendChild(cajaTexto);
-  //cuando se ejecute la f router
+//Boton Limpiar
+const resetButton = document.createElement("button");
+resetButton.textContent = "Borrar";
+resetButton.addEventListener("click", () => {
+  console.log("Borrar");
+});
+rootElement.appendChild(resetButton);
+
+
+
+
