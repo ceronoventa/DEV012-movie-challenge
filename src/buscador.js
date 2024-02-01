288b362fb5554df65d40fffe778d3bd37d8a2dc3
Camila Palma <cpalma.contacto@gmail.com>
2024-02-01 12:34:01 -0300

    botonycaja apikey

--- src/buscador.js
+++ src/buscador.js
@@ -1,15 +1,36 @@
 //contenedor root
 const rootElement = document.getElementById("root");
 
-  //textarea titulo
-  const cajaTexto = document.createElement("textarea");
-  cajaTexto.placeholder = "Titulo";
-  rootElement.appendChild(cajaTexto);
+
+// Input API Key
+const inputApi = document.createElement("input");
+inputApi.type = "text";
+inputApi.placeholder = "Introduce tu API Key";
+rootElement.appendChild(inputApi);
+
+//Boton Apikey
+const api = document.createElement("button");
+api.textContent = "Ingresar";
+api.addEventListener("click", () => {
+  const userApi = inputApi.value;
+if (userApi === "") {
+  alert ("Debes ingresar tu ApiKey");
+} else {
+  localStorage.setItem("KEY", userApi);
+  alert ("¡Cool! Ahora puedes explorar las pelis");
+}
+});
+rootElement.appendChild(api);
+
+//textarea titulo
+const cajaTexto = document.createElement("textarea");
+cajaTexto.placeholder = "Titulo";
+rootElement.appendChild(cajaTexto);
 
 //textarea Año
 const cajaYear = document.createElement("textarea");
-  cajaYear.placeholder = "Año";
-  rootElement.appendChild(cajaYear);
+cajaYear.placeholder = "Año";
+rootElement.appendChild(cajaYear);
 
 // Boton buscar
 const searchButton = document.createElement("button");
@@ -29,5 +50,4 @@ resetButton.addEventListener("click", () => {
 rootElement.appendChild(resetButton);
 
 
-
-
+  
