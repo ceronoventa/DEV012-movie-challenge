f9120be817734672c642e33530ee128817703b90
Camila Palma <cpalma.contacto@gmail.com>
2024-02-20 14:13:34 -0300

    final

--- src/buscador.js
+++ src/buscador.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Contenedor root
+  // Contenedor root obteniendo el elemento con el ID
   const rootElement = document.getElementById("root");
 
-  // Contenedor para textarea y botones
+  // Contenedor para parrafo textarea y botones
   const inputContainer = document.createElement("div");
-  inputContainer.classList.add("input-container"); // Agregar clase para estilos
+  inputContainer.classList.add("input-container"); // Agrega clase para estilos en css
   
+
+  const bienvenida = document.createElement("p");
+bienvenida.textContent = "Bienvenidos! Para comenzar ingresa tu búsqueda a continuación:";
+inputContainer.appendChild(bienvenida);
+
   // Textarea título
   const cajaTexto = document.createElement("textarea");
   cajaTexto.placeholder = "Titulo de tu película...";
@@ -30,6 +35,22 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
   inputContainer.appendChild(searchButton); // Agregar el botón de búsqueda al contenedor de los input
+
+ 
+    // Botón Limpiar
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Borrar";
+    resetButton.addEventListener("click", () => {
+      limpiarTextArea();
+    });
+    inputContainer.appendChild(resetButton);
+    
+    function limpiarTextArea() {
+      cajaTexto.value = "";
+    }
+ 
+ 
+
   rootElement.appendChild(inputContainer); // Agregar el contenedor de textarea y botones al contenedor rootElement
   
   function mostrarResultados(resultados) {
@@ -58,25 +79,24 @@ document.addEventListener("DOMContentLoaded", function () {
         posterPelicula.src = pelicula.Poster;
         contengaTodo.appendChild(posterPelicula);
 
+                  // Botón Home
+const botonHome = document.createElement('button');
+botonHome.textContent = "Home";
+inputContainer.appendChild(botonHome);
+
+
         // Agregar el contenedor principal al rootElement
         rootElement.appendChild(contengaTodo);
-        //
+
       });
+  
+        
+          
+      
     } else {
       const mensaje = document.createElement("p");
       mensaje.textContent = "No se encontraron resultados";
       rootElement.appendChild(mensaje);
     }
-  }
-  // Botón Limpiar
-  const resetButton = document.createElement("button");
-  resetButton.textContent = "Borrar";
-  resetButton.addEventListener("click", () => {
-    limpiarTextArea();
-  });
-
-  inputContainer.appendChild(resetButton);
-  function limpiarTextArea() {
-    cajaTexto.value = "";
   }
 });
